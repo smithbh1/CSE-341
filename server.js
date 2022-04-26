3ec7d8815258fda105a8fa4309f37386381fe090
Brian Smith <smithbh129@gmail.com>
2022-04-25 18:16:18 -0600

    routes

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Josh Dart')
-})
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
