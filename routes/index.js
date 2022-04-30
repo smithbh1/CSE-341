02dc2fc62e28ece5a379428fe7e69aea1a282b68
Brian Smith <smithbh129@gmail.com>
2022-04-30 12:09:20 -0600

    routes fix

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const routes = express.Router();
+const routes = require('express').Router();
 
 routes.get('/', (req, res) =>{
     res.send('Josh Dart')
 });
+
 routes.use('/contacts', require('./contacts'));
+
 module.exports = routes;
